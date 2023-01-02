ed49901b2c94b9fd0c75c92f3de2f8e69e147c05
lilinzhe <1120294646@qq.com>
2023-01-02 19:02:28 +0800

    AJAX 加载CSS JavaScript HTML XML JSON完成

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ loadCss.onclick = ()=>{
     xhr.open('GET','./public/style.css');
     xhr.send();
     xhr.onreadystatechange = ()=>{
-        if(xhr.readyState === 4){
+        if(xhr.readyState === 4 && xhr.status === 200){
             console.log(typeof xhr.response)
             response = xhr.response;
             const style = document.createElement('style');
@@ -13,14 +13,14 @@ loadCss.onclick = ()=>{
             styleTag.insertAdjacentHTML('beforeend',response);
         }
     }
-}
+} 
 //js
 loadJs.onclick = () => {
     const xhr  = new XMLHttpRequest();
     xhr.open('GET','./public/1.js');
     xhr.send();
     xhr.onreadystatechange = function() {
-        if(this.readyState === 4){
+        if(this.readyState === 4 && xhr.status === 200){
             const sc = document.createElement('script');
             const script = document.body.appendChild(sc);
             script.insertAdjacentHTML('beforeend',this.response);
@@ -33,10 +33,42 @@ loadHtml.onclick = () => {
     xhr.open('GET','./public/1.html');
     xhr.send();
     xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4){
+        if(xhr.readyState === 4 && xhr.status === 200){
             const div = document.createElement('div');
             div.innerHTML = xhr.response;
             document.body.appendChild(div);
         }
     }
+}
+//xml
+loadXML.onclick = ()=>{
+    const xhr  = new XMLHttpRequest();
+    xhr.open('GET','./public/1.xml');
+    xhr.send();
+    xhr.onreadystatechange = ()=>{
+        if(xhr.readyState === 4 && xhr.status === 200){
+            const XML = xhr.responseXML;
+            console.log(XML);
+            const warning = XML.getElementsByTagName('warning')[0].textContent.trim();
+            console.log(warning)
+        }
+    }
+}
+
+//JSON
+loadJson.onclick = ()=>{
+    const xhr  = new XMLHttpRequest();
+    xhr.open('GET','./public/1.json');
+    xhr.send();
+    xhr.onreadystatechange = ()=>{
+        if(xhr.readyState === 4 && xhr.status === 200){
+            console.log(xhr.response);
+            console.log(typeof xhr.response);
+            const obj = JSON.parse(xhr.response);
+            console.log(obj);
+            console.log(typeof obj)
+        }
+    }
+        
+    
 }
